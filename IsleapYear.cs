52d71ad568b673b4c6e1ac58ad11c619dc8b7022
kiranravindrant <kiranravindrant@gmail.com>
2021-07-09 15:36:59 +0530

    [kiran]Added IsleapYear or not

---
+++ IsleapYear.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace IsLeapYear
+{
+    class IsLeapYear
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the year in YYYY ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            if ((year % 100 == 0) && (year % 400 == 0) && (year % 4 == 0))
+            {
+                Console.WriteLine("Entered year is a LEAP YEAR");
+            }
+            else
+            {
+                Console.WriteLine("Entered year is not a LEAP YEAR");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
