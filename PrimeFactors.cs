5a30431d3ead96cf7ae06b15062b0327553ec560
kiranravindrant <kiranravindrant@gmail.com>
2021-07-09 23:29:42 +0530

    [kiran]Added:Program to find Prime Factors

---
+++ PrimeFactors.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PrimeFactorsOfN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter the Number to find Prime factors.");
+            n=int.Parse(Console.ReadLine());
+
+            for (int i = 2; i <= n; i++)
+            {
+                while(n % i  == 0)
+                {
+                    Console.Write(i + " ");
+                    n /= i;
+
+                }
+            }
+                Console.ReadLine();
+
+            }
+    }
+}
