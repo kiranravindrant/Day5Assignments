9b9ab2f94d080dae38a36f7b4dc8ab401d79c487
kiranravindrant <kiranravindrant@gmail.com>
2021-07-09 20:33:16 +0530

    [kiran]Added: program to find sum of Harmonic series

---
+++ Harmonic series sum.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace HarmonicSeries
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+            try      //just put exception, incase if user entered something else
+            {
+
+
+                Console.Write("Enter Value of N : ");
+                int n = Convert.ToInt32(Console.ReadLine());
+                //Formula : hn=h*(n-1) + (1/n)
+                double hn = 0;
+                for (double i = 1; i < n; i++)
+                {
+
+                    hn += 1 / i;
+
+                }
+
+               Console.WriteLine("The harmonic Sum is" + hn);
+            } 
+
+            catch(Exception e)                      //for throwing error, instead of exiting from program
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+        }
+    }
+}
