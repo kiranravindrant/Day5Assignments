316c02e6f52e579d1aeb6a2384b2dd2faca34dae
kiranravindrant <kiranravindrant@gmail.com>
2021-07-09 16:51:24 +0530

    [kiran]Added:Power of Two

---
+++ PowerofTwo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PowerOfTwo
+{
+    class PowerofTwo
+    {
+        static void Main()
+        {
+            double result;
+            Console.WriteLine("Enter the Value of N (N less than 31) :");
+            int n = Convert.ToInt32(Console.ReadLine());
+                   
+       
+                for (int i = 1; i < n; i++)
+                {
+                    result = Math.Pow(2, i);
+                    Console.WriteLine(result);
+
+
+                }
+            
+
+        }
+    }
+}
