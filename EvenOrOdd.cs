8cd35f89bc0673011b67182372a77ef09f9c79fe
kiranravindrant <kiranravindrant@gmail.com>
2021-07-09 23:59:56 +0530

    [kiran]Added:Program to check Even or Odd

---
+++ EvenOrOdd.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EvenOrOdd
+{
+    class EvenOrOdd
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number :");
+           int n= int.Parse(Console.ReadLine());
+
+            if (n % 2 == 0)
+            {
+                Console.WriteLine(("Its an Even Number"));
+
+
+            }
+            else
+            {
+                Console.WriteLine("Its  an Odd Number");
+            }
+            Console.ReadKey();
+            
+        }   
+    }
+}
