301c159f27834e5e596786a9b16d8c3711e6aa5c
kiranravindrant <kiranravindrant@gmail.com>
2021-07-12 15:11:54 +0530

    [kiran]Added:Program to swap values of two variables

---
+++ SwapTwoNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SwapTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 10;
+            Console.WriteLine("value of a and b before swap, a = " + a + " b = " + b);
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            
+            Console.WriteLine("values of a and b after  swapping!! a = "+a+" b= "+b);
+            Console.ReadLine();
+
+
+        }
+    }
+}
