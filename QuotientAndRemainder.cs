c3461cdedb64817a919611505500c678ec1e822b
kiranravindrant <kiranravindrant@gmail.com>
2021-07-12 15:56:58 +0530

    [kiran]Added:Program to display Quotient and remainder

---
+++ QuotientAndRemainder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace QuotientAndRemainder
+{
+    class QuotientAndRemainder
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Number");
+            int Num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Divider ");
+            int  Div = Convert.ToInt32(Console.ReadLine());
+
+            int Quotient = (Num / Div);
+            Console.WriteLine("Quotient is = " + Quotient);
+
+            int Remainder = (Num % Div);
+            Console.WriteLine("Remainder is = " + Remainder);
+        }
+    }
+}
